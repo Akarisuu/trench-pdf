4407d72a86e79401015a70ad86cf7326a02afa84
Akarisuu <pawelmajewski@hotmail.com>
2025-02-05 14:31:26 +0100

    feat: github link added

--- src/lib/components/footer/footer.svelte
+++ src/lib/components/footer/footer.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
+	import Github from '$lib/assets/icons/github.svelte';
 	import * as m from '$lib/i18n/messages';
 </script>
 
 <footer
-	class="fixed bottom-0 left-0 w-full text-balance bg-gradient-to-t from-primary/25 to-background to-70% px-4 py-4 text-center text-xs font-light"
+	class="fixed bottom-0 left-0 flex w-full flex-col items-center justify-center gap-2 text-balance bg-gradient-to-t from-primary/25 to-background to-70% px-6 py-4 text-center text-xs font-light sm:flex-row-reverse sm:gap-4"
 >
-	{m.shared_footer_bugInfo()}
-	<a href="https://discordapp.com/users/226362049196720128" class="font-bold"
-		>{m.shared_footer_discord()}</a
-	>
+	<p>
+		<span>
+			{m.shared_footer_bugInfo()}
+		</span>
+		<a href="https://discordapp.com/users/226362049196720128" class="font-bold"
+			>{m.shared_footer_discord()}</a
+		>
+	</p>
+
+	<div class="hidden h-5 w-px bg-foreground sm:block"></div>
+
+	<a href="https://github.com/Akarisuu/trench-pdf" target="_blank">
+		<Github class="w-5 fill-foreground hover:fill-foreground/80" />
+	</a>
 </footer>
